8c2febcd60775bae3319ae5c59166cd2099a0e0e
chrismitchellcs <chrismitchellcs@gmail.com>
2025-06-03 20:43:08 -0700

    updated for production

--- server/api/index.js
+++ server/api/index.js
@@ -73,10 +73,8 @@ app.use(
     }),
     cookie: {
       httpOnly: true,
-      // secure: process.env.NODE_ENV === "production",
-      secure: false,
-      // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 1000 * 60 * 60 * 2,
     },
   })
